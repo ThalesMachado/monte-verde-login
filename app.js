338e178686c2ed631c64ac1afa067e75555d1d40
Thales Machado <TR030149@minhati.com.br>
2024-01-30 12:31:14 -0300

    Incluindo dependencias

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 3000
+
+
+const passport = require('passport')
+
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({extended: true}));
+app.use(require('express-session')({secret: 'tudo tranquilo', resave: true, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
