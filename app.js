06c34894c0238ff35a463a25013ddd0fd4ada0e1
Thales Machado <TR030149@minhati.com.br>
2024-01-30 16:23:59 -0300

    Rotas iniciais

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const app = express()
 
+const routes = require('./src/routes')
 
 const passport = require('passport')
 
+const port = process.env.PORT || 3000;
+
+app.use(require('morgan')('dev'))
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({extended: true}));
 app.use(require('express-session')({secret: 'tudo tranquilo', resave: true, saveUninitialized: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+router.route('/').get((req, res) => {
+    res.send('Hello World!')
+})
+
+router.route('/test').get((req, res) => {
+    res.send('Inhaí')
+})
+
+module.exports = router
